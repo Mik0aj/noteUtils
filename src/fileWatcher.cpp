a0f1f0873b20b8808e83987f44ef26ec6e716b99
Mikołaj Aleksander Mosoń <mikoajmoson@gmail.com>
2022-09-04 18:01:34 +0200

    converted inotify example into class, removed header fileWatcher.h because now it's a template class

--- src/fileWatcher.cpp
+++ src/fileWatcher.cpp
@@ -1,83 +1,118 @@
-//
-// Created by mikoaj on 03.09.22.
-//
 
-#include "fileWatcher.h"
+#include <iostream>
+#include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <map>
 
-
-/* Read all available inotify events from the file descriptor 'fd'.
-   wd is the table of watch descriptors for the directories in argv.
-   argc is the length of wd and argv.
-   argv is the list of watched directories.
-   Entry 0 of wd and argv is unused. */
-
-static void handle_events(int fd, int *wd, int argc, char *argv[]) {
+template<typename Func>
+class fileWatcher {
+    int fileDescriptor;
+    int *watchDescriptor;
     /* Some systems cannot read integer variables if they are not
        properly aligned. On other systems, incorrect alignment may
        decrease performance. Hence, the buffer used for reading from
        the inotify file descriptor should have the same alignment as
        struct inotify_event. */
-
-    char buf[4096]
-            __attribute__ ((aligned(__alignof__(struct inotify_event))));
+    char buf;
     const struct inotify_event *event;
+    int i;
+    int poll_num;
     ssize_t len;
+    nfds_t nfds;
+    struct pollfd fds[2];
+    std::map<int, Func> fileDescriptorFunctionMap;
 
-    /* Loop while events can be read from inotify file descriptor. */
-
-    for (;;) {
-
-        /* Read some events. */
-
-        len = read(fd, buf, sizeof(buf));
-        if (len == -1 && errno != EAGAIN) {
-            perror("read");
-            exit(EXIT_FAILURE);
+public:
+    fileWatcher(const std::vector<std::string> &paths,std::map<int, Func> map) {
+        fileDescriptor = inotify_init1(IN_NONBLOCK);
+        if (fileDescriptor == -1) {
+            throw "inotify_init1 error";
         }
+        fileDescriptorFunctionMap=map;
+        updatePaths(paths);
+    }
 
-        /* If the nonblocking read() found no events to read, then
-           it returns -1 with errno set to EAGAIN. In that case,
-           we exit the loop. */
-
-        if (len <= 0)
-            break;
-
-        /* Loop over all events in the buffer. */
-
-        for (char *ptr = buf; ptr < buf + len;
-             ptr += sizeof(struct inotify_event) + event->len) {
-
-            event = (const struct inotify_event *) ptr;
-
-            /* Print event type. */
-
-            if (event->mask & IN_OPEN)
-                printf("IN_OPEN: ");
-            if (event->mask & IN_CLOSE_NOWRITE)
-                printf("IN_CLOSE_NOWRITE: ");
-            if (event->mask & IN_CLOSE_WRITE)
-                printf("IN_CLOSE_WRITE: ");
-
-            /* Print the name of the watched directory. */
-
-            for (int i = 1; i < argc; ++i) {
-                if (wd[i] == event->wd) {
-                    printf("%s/", argv[i]);
-                    break;
+    void handle_events() {
+        char bufArr[4096] __attribute__ ((aligned(__alignof__(struct inotify_event))));
+        /* Loop while events can be read from inotify file descriptor. */
+        for (;;) {
+            /* Read some events. */
+            len = read(fileDescriptor, bufArr, sizeof(bufArr));
+            if (len == -1 && errno != EAGAIN) {
+                perror("read");
+                exit(EXIT_FAILURE);
+            }
+            /* If the nonblocking read() found no events to read, then
+               it returns -1 with errno set to EAGAIN. In that case,
+               we exit the loop. */
+            if (len <= 0)
+                break;
+            /* Loop over all events in the buffer. */
+            for (char *ptr = bufArr; ptr < bufArr + len; ptr += sizeof(struct inotify_event) + event->len) {
+                event = (const struct inotify_event *) ptr;
+                for (const auto & [mask, function]: fileDescriptorFunctionMap) {
+                    if (event->mask & mask) function();
                 }
             }
-
-            /* Print the name of the file. */
-
-            if (event->len)
-                printf("%s", event->name);
-
-            /* Print type of filesystem object. */
-
-            if (event->mask & IN_ISDIR)
-                printf(" [directory]\n");
-            else
-                printf(" [file]\n");
         }
     }
-}
+
+    void updatePaths(const std::vector<std::string> &paths) {
+        watchDescriptor = static_cast<int *>(calloc(paths.size(), sizeof(int)));
+        if (watchDescriptor == nullptr) {
+            throw "calloc error";
+        }
+        for (i = 0; i < paths.size(); i++) {
+            watchDescriptor[i] = inotify_add_watch(fileDescriptor, (const char *) paths[i].c_str(), IN_OPEN | IN_CLOSE);
+            if (watchDescriptor[i] == -1) {
+                throw "Cannot watch " + paths.at(i);
+            }
+        }
+    }
+
+    void loop() {
+        std::cout << "Press ENTER key to terminate.\n";
+        nfds = 2;
+
+
+        fds[0].fd = STDIN_FILENO;       /* Console input */
+        fds[0].events = POLLIN;
+
+        fds[1].fd = fileDescriptor;     /* Inotify input */
+        fds[1].events = POLLIN;
+        std::cout << "Listening for events.\n";
+
+        while (1) {
+            poll_num = poll(fds, nfds, -1);
+            if (poll_num == -1) {
+                if (errno == EINTR)
+                    continue;
+                throw "poll";
+            }
+            if (poll_num > 0) {
+                if (fds[0].revents & POLLIN) {
+                    /* Console input is available. Empty stdin and quit. */
+                    while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
+                        continue;
+                    break;
+                }
+                if (fds[1].revents & POLLIN) {
+                    /* Inotify events are available. */
+                    handle_events();
+                }
+            }
+        }
+        std::cout << "Listening for events stopped.\n";
+    }
+
+    ~fileWatcher() {
+        close(fileDescriptor);
+        free(watchDescriptor);
+    }
+};
